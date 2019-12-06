6d3b2e4969da8ebb8b2f2d605e7cf6094cd64c7f
Michael Zion <noisleahcim@gmail.com>
2019-12-06 02:24:33 +0200

    Rename project to shlib

--- cmd/root.go
+++ cmd/root.go
@@ -29,7 +29,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "shez",
+	Use:   "shlib",
 	Short: "CLI Short Description",
 	Long:  `CLI Long Description`,
 	// Uncomment the following line if your bare application
@@ -53,7 +53,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.shez.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.shlib.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -73,9 +73,9 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".shez" (without extension).
+		// Search config in home directory with name ".shlib" (without extension).
 		viper.AddConfigPath(home)
-		viper.SetConfigName(".shez")
+		viper.SetConfigName(".shlib")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
